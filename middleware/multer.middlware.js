a5ee3deb28054360fd1e87b874046a8de52ea741
AYUSH MISHRA <109171038+AyushMishra2003@users.noreply.github.com>
2024-12-31 22:20:34 +0530

    Update multer.middlware.js

--- middleware/multer.middlware.js
+++ middleware/multer.middlware.js
@@ -33,7 +33,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 2 * 1024 * 1024 } // 2MB file size limit
+  limits: { fileSize: 5 * 1024 * 1024 } // 5MB file size limit
 });
 
 export default upload;  // Export the multer configuration
